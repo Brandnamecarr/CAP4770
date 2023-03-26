a8bef85668de273df9f3c8815b40f0495ea49ab7
vmarlowe <77749581+vmarlowe@users.noreply.github.com>
2023-03-26 11:15:20 -0400

    Added de-duplicate code. And structure for proc

--- operations.py
+++ operations.py
@@ -2,3 +2,11 @@
     File contains the code for the logic of the application
 '''
 import pandas as pd
+
+df = pd.read_csv('data\original\survey_results_public.csv')
+df_unique = df.drop_duplicates()
+df_unique.to_csv('data\cleaned\data-only-unique-rows.csv', index=False)
+
+# QA check - export identified duplicates to a new CSV
+df_duplicates = df[~df.index.isin(df_unique.index)]
+df_duplicates.to_csv('data\extracted\data_removed_duplicates.csv', index=False)
